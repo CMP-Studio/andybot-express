52c261e7eb42a8565c9bff8acb01601faba0ebee
Abhishek Chadha <abhishekchadha235@gmail.com>
2018-06-04 14:05:39 -0400

    scavenger hunt, scan updates, cleanup

---
+++ config.js
@@ -0,0 +1,17 @@
+const defaultConfig = {
+    google: {
+        apiKey: process.env.BOTS_GOOGLE_API_KEY
+    },
+    scheduleCalendarId: process.env.BOTS_GOOGLE_CALENDAR_ID,
+};
+
+const config = {
+    development: {
+        ...defaultConfig,
+    },
+    test: {
+        ...defaultConfig,
+    }
+};
+
+module.exports = config[process.env.NODE_ENV || 'development'];
